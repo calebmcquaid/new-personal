43a0a79425332538adefe8b8609832c1bf4081fc
Caleb McQuaid <McQuaid_Caleb@bah.com>
2019-05-21 00:43:13 -0400

    changed config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  plugins: [`gatsby-plugin-styled-components`],
-  plugins: [
+  plugins: [`gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
       options: {
