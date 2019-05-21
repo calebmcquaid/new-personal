b2b9492d7e76abcb09bbd7f7c483815b7281918a
Caleb McQuaid <McQuaid_Caleb@bah.com>
2019-05-21 11:32:06 -0400

    added favicon plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,36 @@
 module.exports = {
   plugins: [`gatsby-plugin-styled-components`,
-    {
+  {
+    resolve: `gatsby-plugin-favicon`,
+    options: {
+      logo: "./src/favicon.png",
+
+      // WebApp Manifest Configuration
+      appName: null, // Inferred with your package.json
+      appDescription: null,
+      developerName: 'Caleb McQuaid',
+      developerURL: null,
+      dir: 'auto',
+      lang: 'en-US',
+      background: '#fff',
+      theme_color: '#fff',
+      display: 'standalone',
+      orientation: 'any',
+      version: '1.0',
+
+      icons: {
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        coast: false,
+        favicons: true,
+        firefox: true,
+        yandex: false,
+        windows: false
+      }
+    }
+  },
+  {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
       options: {
         fonts: [
