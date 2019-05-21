6c14c475be2461fabe2e147dcfa6bd8669035234
Caleb McQuaid <McQuaid_Caleb@bah.com>
2019-05-21 12:15:15 -0400

    adding GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,36 +1,53 @@
 module.exports = {
-  plugins: [`gatsby-plugin-styled-components`,
-  {
-    resolve: `gatsby-plugin-favicon`,
-    options: {
-      logo: "./src/favicon.png",
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/favicon.png",
 
-      // WebApp Manifest Configuration
-      appName: null, // Inferred with your package.json
-      appDescription: null,
-      developerName: 'Caleb McQuaid',
-      developerURL: null,
-      dir: 'auto',
-      lang: 'en-US',
-      background: '#fff',
-      theme_color: '#fff',
-      display: 'standalone',
-      orientation: 'any',
-      version: '1.0',
+        // WebApp Manifest Configuration
+        appName: null, // Inferred with your package.json
+        appDescription: null,
+        developerName: "Caleb McQuaid",
+        developerURL: null,
+        dir: "auto",
+        lang: "en-US",
+        background: "#fff",
+        theme_color: "#fff",
+        display: "standalone",
+        orientation: "any",
+        version: "1.0",
 
-      icons: {
-        android: true,
-        appleIcon: true,
-        appleStartup: true,
-        coast: false,
-        favicons: true,
-        firefox: true,
-        yandex: false,
-        windows: false
-      }
-    }
-  },
-  {
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          yandex: false,
+          windows: false,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-140598401-1", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared accross all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+      },
+    },
+    {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
       options: {
         fonts: [
