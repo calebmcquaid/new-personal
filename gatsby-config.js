6167f75881544ad4dd1e7b149855d384d1b1e600
Caleb <calebmcquaid@Josephs-MacBook-Pro.local>
2020-07-10 00:01:20 -0400

    start of spotify

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,17 @@
+const path = require(`path`)
+
 module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    `gatsby-transformer-sharp`, 
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-favicon`,
       options: {
--- src/pages/zen.js
+++ src/pages/zen.js
@@ -2,7 +2,8 @@ import React from 'react'
 import Item from '../components/Item'
 import Header from '../components/Header'
 import styled from 'styled-components'
-import {graphql} from 'gatsby'
+import {graphql, useStaticQuery} from 'gatsby'
+import Img from 'gatsby-image'
 
 const Container = styled.div`
     margin: 50px 100px;
@@ -16,44 +17,81 @@ const Container = styled.div`
 
 
 
-const Zen = ({data}) => {
-    const {spot} = data
+const Zen = () => {
+//     const {spot} = data
+//     return (
+//     <div>
+//         <Header />
+//         <Container>
+//             {spot.edges.map(({ node: artist }) => (
+//           <div>
+//               <h1>{artist.name}</h1>
+//               <p>{artist.genre}</p>
+//           </div>
+//             ))}
+            
+//             <h1>Learning:</h1>
+//             <Item title="Testing" description="Hello this is also a test"/>
+//             <h1>Watching:</h1>
+//             <Item title="Watching" description="This is description"/>
+//             <h1>Listening:</h1>
+//             <Item title="Podcast" description="Syntax.fm" />
+//         </Container>
+//     </div>
+//     )
+// }
+
+const data = useStaticQuery(graphql`
+        query SpotifyTopArtistsQuery {
+            allSpotifyTopArtist(
+                filter: { time_range: { eq: "short_term" } }
+                sort: { fields: order }
+              ) {
+                edges {
+                  node {
+                    name
+                    genres
+                    image {
+                        localFile {
+                        childImageSharp {
+                          fixed(width: 100) {
+                            ...GatsbyImageSharpFixed_withWebp
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+        }
+    `)
     return (
-    <div>
-        <Header />
-        <Container>
-            <h1>{spot.name}</h1>
-            {spot.edges.map(({ node: playlistTrack }) => (
-          <div><h1>{playlistTrack.name}</h1> 
-          </div>
-            ))}
-            {/* {data.allSpotifyTopArtist.edges.node.name} */}
-            <h1>Learning:</h1>
-            <Item title="Testing" description="Hello this is also a test"/>
-            <h1>Watching:</h1>
-            <Item title="Watching" description="This is description"/>
-            <h1>Listening:</h1>
-            <Item title="Podcast" description="Syntax.fm" />
-        </Container>
-    </div>
+        <>
+            { data.allSpotifyTopArtist.edges &&
+                <div className="spotify__top-artists">
+                    <h2>Top 20 Artists</h2>
+                    <p>
+                        These are my top 20 artists from the past four weeks.
+                    </p>
+                    <div className="spotify__list">
+                        {data.allSpotifyTopArtist.edges.map((artist, index) =>
+                            <div key={index} className="spotify__list-item d-flex">
+                                <div className="spotify__album-cover">
+                                    <Img
+                                        fixed={artist.node.image.localFile.childImageSharp.fixed} 
+                                    />
+                                </div>
+                                <h3>
+                                    <span className="spotify__list-item-number">{index + 1}</span>
+                                    { artist.node.name }
+                                </h3>
+                            </div>
+                        )}
+                    </div>
+                </div>
+          }
+        </>
     )
 }
-
-export const query = graphql`
-    query Spotify{
-        spot: allSpotifyTopArtist(
-      filter: { time_range: { eq: "medium_term" } }
-      sort: { fields: order }
-    ) {
-      edges {
-        node {
-          name
-          genres
-          
-        }
-      }
-    }
-  }
-`
 
 export default Zen
